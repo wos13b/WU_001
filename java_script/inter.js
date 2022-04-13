9a79508a4de885b5a7143a9591e34cdd47c4e2f6
wesley oliveira <96589369+wos13b@users.noreply.github.com>
2022-04-13 19:12:45 -0300

    Atualização

--- java_script/inter.js
+++ java_script/inter.js
@@ -1,5 +1,5 @@
-var n1 = Number(window.prompt('digite um numero'))
+/*var n1 = Number(window.prompt('digite um numero'))
 var n2 = Number(window.prompt('digite outro numero'))
 r = n1 + n2
-window.alert(`a soma entre ${n1} + ${n2} e ${r}`)
+window.alert(`a soma entre ${n1} + ${n2} e ${r}`)*/
 
