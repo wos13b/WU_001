e52dc47ccd258ca90b9b7927b970fb4f98a3abef
wesley oliveira <96589369+wos13b@users.noreply.github.com>
2022-04-23 17:57:58 -0300

    Atualização_005

---
+++ java_script/inter.js
@@ -0,0 +1,12 @@
+function mouse_on(){
+
+    deslize = window.document.getElementById('img_prime')
+    deslize.innerText = 'mouse_on'
+
+}
+function mouse_off(){
+
+    deslize = window.document.getElementById('img_prime')
+    deslize.innerText = 'mouse_off'
+
+}
