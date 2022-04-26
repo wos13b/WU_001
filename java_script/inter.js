dd97f7104d43fe8c74c1da343c82936c82ecad33
wesley oliveira <96589369+wos13b@users.noreply.github.com>
2022-04-26 12:00:18 -0300

    Atualização_10

--- java_script/inter.js
+++ java_script/inter.js
@@ -1,10 +1,24 @@
 deslize = window.document.getElementById('img_prime')
-deslize.addEventListener('mouseenter', mouseon)
+deslize.addEventListener('mouseenter', mouse_on)
+deslize.addEventListener('mouseout', mouse_off)
+deslize.addEventListener('click', mouse_click)
 
 
-function mouseon(){
 
-    deslize = window.document.getElementById('img_prime')
+
+function mouse_on(){
+
     deslize.innerText = 'mouse_on'
+
+}
+function mouse_off(){
+
+    deslize.innerText = 'mouse_off'
+
+}
+function mouse_click(){
+
+    deslize.style.background = 'red'
+    deslize.innerText = 'click'
 
 }
