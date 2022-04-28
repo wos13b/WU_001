dd48e333bdf1c8d64ec4edef1d28767c824cb2e4
wesley oliveira <96589369+wos13b@users.noreply.github.com>
2022-04-28 17:06:38 -0300

    Atualização_13

--- java_script/inter.js
+++ java_script/inter.js
@@ -1,36 +1,23 @@
-deslize = window.document.getElementById('img_prime')
-somar = document.getElementById('somar')
-res = document.getElementById('res')
-/*deslize.addEventListener('mouseenter', mouse_on)
-deslize.addEventListener('mouseout', mouse_off)
-deslize.addEventListener('click', mouse_click)*/
-somar.addEventListener('click', sm_click)
-res = document.getElementById('res')
+result = document.getElementById('resultado')
+result.addEventListener('click', sm_click)
 
 
 
-/*function mouse_on(){
-
-    deslize.innerText = 'mouse_on'
-
-}
-function mouse_off(){
-
-    deslize.innerText = 'mouse_off'
-
-}
-function mouse_click(){
-
-    deslize.style.background = 'red'
-    deslize.innerText = 'click'
-
-}*/
 function sm_click(){
 
-    var n2 = Number(window.document.getElementById('sm1').value)
-    var n1 = Number(window.document.getElementById('sm2').value)
-    var s = n1 + n2
-    res.innerHTML = s
+    test = document.getElementById('A')
+    n = Number(window.document.getElementById('sm1').value)
+    if (n > 0){
+
+        test.innerText = 'positivo'
+
+    }
+    else{
+
+        test.innerText = 'negativo'
+
+    }
+    
 
 }
 
