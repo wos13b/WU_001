1f564225b6c166bd01c67a20cf096db273c984c3
wesley oliveira <96589369+wos13b@users.noreply.github.com>
2022-05-06 12:58:36 -0300

    atualização

--- java_script/inter.js
+++ java_script/inter.js
@@ -3,7 +3,9 @@ const b_menu = document.getElementById('btn_menu')
 function click(){
 
     const nav = document.getElementById('navegation')
+    const btn_img = document.getElementById('btn_img')
     nav.classList.toggle('active')
+    btn_img.classList.toggle('active')
 
 }
 
