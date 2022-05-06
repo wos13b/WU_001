1c5c5d2a5b5c5151a759764ef065a45e3769230c
wesley oliveira <96589369+wos13b@users.noreply.github.com>
2022-05-06 19:17:24 -0300

    a

--- java_script/inter.js
+++ java_script/inter.js
@@ -1,14 +1,22 @@
-const b_menu = document.getElementById('btn_menu')
+const b_profile = document.getElementById('btn_menu')
+const b_options = document.getElementById('btn_options')
 
-function click(){
+function click_1(){
 
     const nav = document.getElementById('navegation')
-    const btn_img = document.getElementById('btn_img')
+    const btn_img_profiles = document.getElementById('btn_img-profiles')
     nav.classList.toggle('active')
-    btn_img.classList.toggle('active')
+    btn_img_profiles.classList.toggle('active')
+
+}
+function click_2(){
+
+    const btn_img_options = document.getElementById('btn_img-options')
+    btn_img_options.classList.toggle('active')
+
 
 }
 
-
-b_menu.addEventListener('click', click)
+b_profile.addEventListener('click', click_1)
+b_options.addEventListener('click', click_2)
 
