f657ed7d3ca1016735c9251e61290a266c9f3161
wesley oliveira <96589369+wos13b@users.noreply.github.com>
2022-05-07 13:32:09 -0300

    Atualização

--- java_script/inter.js
+++ java_script/inter.js
@@ -1,19 +1,20 @@
-const b_profile = document.getElementById('btn_menu')
+const b_profile = document.getElementById('btn_profiles')
 const b_options = document.getElementById('btn_options')
 
 function click_1(){
 
-    const nav = document.getElementById('navegation_profiles')
+    const nave_prf = document.getElementById('navegation_profiles')
     const btn_img_profiles = document.getElementById('btn_img-profiles')
-    nav.classList.toggle('active')
+    nave_prf.classList.toggle('active')
     btn_img_profiles.classList.toggle('active')
 
 }
 function click_2(){
 
-    const btn_img_options = document.getElementById('btn_img-options')
-    btn_img_options.classList.toggle('active')
-
+    const nave_option  = document.getElementById('navegation_options')
+    const btn_options = document.getElementById('btn_img-options')
+    nave_option.classList.toggle('active')
+    btn_options.classList.toggle('active')
 
 }
 
